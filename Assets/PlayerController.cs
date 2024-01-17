a5f94809ef34750fb30cc7668eadb942d3991cc5
Björn Granberg <21grabjo@soltorgsgymnasiet.se>
2024-01-17 11:21:34 +0100

    movemnt part 2
    UNFINISHED!

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,22 +4,20 @@ using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using UnityEngine.Serialization;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     private Transform _transform;
     private Rigidbody2D _rb;
     
-    [SerializeField] float movementSpeed = 1;
+    [SerializeField] float _movemntSpeed = 1;
 
 
     void Awake()
     {
         _transform = transform;
-
         _rb = GetComponent<Rigidbody2D>();
-
-        
     }
 
     void Start()
@@ -27,9 +25,23 @@ public class NewBehaviourScript : MonoBehaviour
         
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
-        print("PlayerInput");
+        Vector2 v = _rb.velocity; 
+        v.x = _movemntSpeed;
+        if (_movemntSpeed < -.1f)
+        {
+            transform.right = Vector3.left;
+        }
+        if (_movemntSpeed > .1f)
+        {
+            transform.right = Vector3.right;
+        }
+        float animationSpeed = Mathf.Abs(_movemntSpeed);
+    }
+    void OnMove(InputValue v)
+    {
+        print("test1");
+        _movemntSpeed = v.Get<Vector2>().x * 5;
     }
 }
