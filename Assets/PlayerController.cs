1c251f4fa53743ddd4d7d3141e3c39c3441839e2
Björn Granberg <21grabjo@soltorgsgymnasiet.se>
2024-01-31 10:23:55 +0100

    movement sys 2 part 2
    TODO: Fine tincker with values

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -15,6 +15,7 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] Vector2 _movemntSpeed = Vector2.zero; //Misleading name, changing this vector will give the player an initial movement force when spawning. See movement multiplier. 
     [SerializeField] private float top_speed = 5;
     [SerializeField] private float acceleration = 0.1f;
+    [SerializeField] private float deacceleration = 0.1f;
     
     
 
@@ -32,9 +33,19 @@ public class NewBehaviourScript : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (!Mathf.Approximately(_rb.velocity.x, top_speed) &&! Mathf.Approximately(_movemntSpeed.x, 0)) 
+        //The following code is responsible for check if the movement keys get pressed and calculating and adding movement speed
+        if (!Mathf.Approximately(_rb.velocity.x, top_speed) &&! Mathf.Approximately(_movemntSpeed.x, 0)) //If velocity In X-axis =/= Topspeed, && If button is not un-pressed
         {
             _rb.AddForce(Vector2.right * (acceleration * Mathf.Sign(_movemntSpeed.x)));
+        }
+        if (Mathf.Approximately(_movemntSpeed.magnitude, 0))
+        {
+            Debug.Log("penus1");
+            _rb.drag = 100;
+        }
+        else
+        {
+            _rb.drag = 0;
         }
     }
     void OnMove(InputValue v)
