a6a0ab98ad5285fa9f572dfc44e3f73f6a252e69
Filip Eriksson <21erifil@soltorgsgymnasiet.se>
2024-02-14 11:02:18 +0100

    Dragon Shoot 1
    * Just preparations for the shooting mechanics for the dragon

---
+++ Assets/Scripts/DragonShoot.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonShoot : MonoBehaviour
+{
+
+    public GameObject projectile;
+    public Transform projectilePos;
+
+    private float timer;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 2)
+        {
+            timer = 0;
+            shoot();
+        }
+    }
+
+    void shoot()
+    {
+        Instantiate(projectile, projectilePos.position, Quaternion.identity);
+    }
+}
