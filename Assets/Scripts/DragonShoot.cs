6deb450caf6c43e25e3d468e0ea8708213043861
Filip Eriksson <21erifil@soltorgsgymnasiet.se>
2024-02-21 11:06:27 +0100

    Dragon head rotation
    * Made it so the dragon head now rotates towards a target gameObject (player).

--- Assets/Scripts/DragonShoot.cs
+++ Assets/Scripts/DragonShoot.cs
@@ -1,14 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DragonShoot : MonoBehaviour
 {
-
+    // For the dragon shooting mechanics 
     public GameObject projectile;
     public Transform projectilePos;
-
+    
     private float timer;
+    
+    //For rotating dragon head towards player
+    public GameObject rotateTowards;
+    public float speed;
+    public float rotationModifier;
+    
+    
+ 
     void Start()
     {
         
@@ -16,12 +25,26 @@ public class DragonShoot : MonoBehaviour
 
     void Update()
     {
+        //Shooting
         timer += Time.deltaTime;
 
         if (timer > 2)
         {
             timer = 0;
             shoot();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        //Dragon head rotation towards player
+        if(rotateTowards != null)
+        {
+            Vector3 vectorToTarget = rotateTowards.transform.position - transform.position;
+            float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - rotationModifier;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
         }
     }
 
