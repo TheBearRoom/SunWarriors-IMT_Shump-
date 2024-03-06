bfc63898ae478a8676ec8bd7a52b9093212783c1
Filip Eriksson <21erifil@soltorgsgymnasiet.se>
2024-03-06 11:13:05 +0100

    Vertebrae Rotation Start
    *Laid out the groundwork for vertebrae rotation

---
+++ Assets/Scripts/VertebraeRotation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vertebrae : MonoBehaviour
+{
+    public GameObject rotateTowards;
+    public float speed;
+    public float rotationModifier;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    
+    private void FixedUpdate()
+    {
+        //Dragon head rotation towards player
+        if(rotateTowards != null)
+        {
+            Vector3 vectorToTarget = rotateTowards.transform.position - transform.position;
+            float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - rotationModifier;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
+        }
+    }
+}
